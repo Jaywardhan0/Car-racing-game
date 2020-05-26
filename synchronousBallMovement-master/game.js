b9e76c73991970fbd7ec86ce4495053853c67d29
Jaywardhan0 <62792367+Jaywardhan0@users.noreply.github.com>
2020-05-26 18:56:31 +0530

    Add files via upload

---
+++ synchronousBallMovement-master/game.js
@@ -0,0 +1,16 @@
+class Game{
+constructor(){
+
+}
+getState(){
+    var gameStateref= database.ref("gameState")
+    gameStateref.on("value",function(data){
+    gameState= data.val();
+    })
+}
+update(state){
+    database.ref('/').update({
+        gameState:state
+    })
+}
+}
---
+++ synchronousBallMovement-master/sketch.js
@@ -0,0 +1,8 @@
+var form
+function setup(){
+  createCanvas(400,400)
+}
+function draw(){
+form = new Form();
+form.display(); 
+}
